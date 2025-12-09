84aa53e32cfb0d860f33e1ee9e7a44e3b993796c
gueverstarstone <josephmungai028@gmail.com>
2025-12-09 15:59:13 +0300

    workerd on improving my skills

---
+++ day2/substring.js
@@ -0,0 +1,14 @@
+//use of index of to find the position of a string
+const message = "Welcome to my world";
+const position = message.indexOf("any");
+console.log(position);//returnss -1 if no word is found
+
+//providing a second argument to find the position
+const message2 = 'I am a lone walker in this jungle'
+const position2 = message2.indexOf('This',5)
+console.log(position2);
+
+
+const str = "I am learning JavaScript.";
+str.indexOf("Javascript");
+console.log(str);
---
+++ day2/tempelete.js
@@ -0,0 +1,6 @@
+//working with tempelete literals and string interpolation
+const name = "Gueverstarstone";
+const age = 25;
+const message = `My name is ${name}, and I am ${age} years old.`;
+console.log(message);
+
