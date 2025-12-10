1653a3a148ba0b60c4b8c31d8b2f606358df92ca
gueverstarstone <josephmungai028@gmail.com>
2025-12-10 13:22:46 +0300

    working with slice method

---
+++ day3/slice.js
@@ -0,0 +1,6 @@
+// slice() method - allows you to extract a portion of a string and returns a new string, without modifying the original string.
+// It takes two parameters: the starting index and the optional ending index.
+let message ='Welcome home'
+let name = message.slice(-3)
+console.log(name);
+console.log(typeof null);
