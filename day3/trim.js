7f61024a70bdf791840efd3a69af04323b2f9896
gueverstarstone <josephmungai028@gmail.com>
2025-12-10 15:43:40 +0300

    worked with trim()

---
+++ day3/trim.js
@@ -0,0 +1,16 @@
+//trim() method
+let name = 'jose se ph   '
+let newName = name.trim()
+console.log(newName);
+
+//trimStart()
+let greeting = "   Hello!   ";
+console.log(greeting);  // "   Hello!   "
+let trimmedStart = greeting.trimStart();
+console.log(trimmedStart);  // "Hello!   "
+
+//trimEnd()
+let message = "   Wantam!   ";
+console.log(message);  // "   Wantam!   "
+let trimmedEnd = message.trimEnd();
+console.log(trimmedEnd);  // "   Wantam!"
---
+++ day3/uppercase.js
@@ -0,0 +1,10 @@
+// toUpperCase() method converts all the characters to uppercase letters and returns a new string with all uppercase characters
+
+let myName = "joseph";
+let newName = myName.toUpperCase();
+console.log(newName);
+
+// toLowerCase() method converts all characters in a string to lowercase.
+let lastName = "GUEVErstarstoNE";
+let message = lastName.toLowerCase()
+console.log(message);
