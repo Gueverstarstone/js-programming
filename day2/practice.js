8c0060391f7e082f2adf2260b5b483dbe2e83460
gueverstarstone <josephmungai028@gmail.com>
2025-12-11 11:16:50 +0300

    did some practice of the previous day lessons

---
+++ day2/practice.js
@@ -0,0 +1,7 @@
+let myName = 'mungai'
+let message = `welcome home ${myName}`
+console.log(message.indexOf('welcome'))
+
+let firstName = 'Rigathiless wambua'
+let newName = firstName.slice(2,7)
+console.log(newName);
