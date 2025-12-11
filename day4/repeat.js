88594acbba81d351c339c46b7d6a94c07990cdb2
gueverstarstone <josephmungai028@gmail.com>
2025-12-11 12:42:46 +0300

    working with repeat and replace methods

---
+++ day4/repeat.js
@@ -0,0 +1,5 @@
+// repeat() method is a built-in function in JavaScript that allows you to repeat a string a specified number of times.
+let name = "joe,";
+let newName = name.repeat(3)
+console.log(newName);
+
---
+++ day4/replace.js
@@ -0,0 +1,4 @@
+// replace() method in JavaScript allows you to find a specified value (like a word or character) in a string and replace it with another value. 
+let message = 'welcome to nairobi'
+let newMsg = message.replace('nairobi','nakuru')
+console.log(newMsg);
